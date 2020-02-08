4768614df093a68cb1b37e2a75a2472555f48851
prasanna <designerprasan007@gmail.com>
2020-02-08 18:59:52 +0530

    updated aws

--- aws_new.sh
+++ aws_new.sh
@@ -5,5 +5,5 @@ echo "opening new aws server."
 
 
 
-ssh  -i ../../..//inform.pem ubuntu@ec10-20-10-33-2.us-east-1.compute.amazonaws.com
+ssh  -i ../../../key_name   ubuntu@ec10.20.30.40.us-east-1.compute.amazonaws.com
 
